3ee369c7b09b0c9a31db2ac49b9c1a5c6a6f434d
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-31 22:10:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,9 +19,9 @@ def summon_captain_planet(group)
 end
 
 # code an argument here
-
+# Your code here
 def long_planeteer_calls
-  # Your code here
+  
 end
 
 def find_the_cheese# code an argument here

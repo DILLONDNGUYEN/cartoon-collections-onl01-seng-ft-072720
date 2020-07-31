862201fdef785006f81d380fb80b1ea87e02a2ae
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-31 22:32:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,9 +31,10 @@ def long_planeteer_calls(words)
   end
 end
 
-
+# code an argument here
 # the array below is here to help
-def find_the_cheese# code an argument here
+
+def find_the_cheese
   
   cheese_types = ["cheddar", "gouda", "camembert"]
 end

6742bbf9c305a7434f99628bbb9c26948477de1b
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-31 22:30:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ assorted_words = ["two", "go", "industrious", "bop"]
 
 def long_planeteer_calls(words)
   words.any? do |word|
-  word.length 
+  word.length > 4
 end
 
 def find_the_cheese# code an argument here

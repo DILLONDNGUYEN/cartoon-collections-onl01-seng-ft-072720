6642f179b371fa6fb3c0fa4ace77970235c9b659
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-31 22:06:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(planeteer_calls)
   planeteer_calls = []
-  planeteer_calls.
+  planeteer_calls.map
   
 end
 
